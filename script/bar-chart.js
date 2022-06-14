3450103d6cbc1fe4b538bc46508f852d38fef42b
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2022-06-14 02:08:22 -0700

    Added basic functionality

---
+++ script/bar-chart.js
@@ -0,0 +1,65 @@
+let data = [
+    {
+      "day": "mon",
+      "amount": 17.45
+    },
+    {
+      "day": "tue",
+      "amount": 34.91
+    },
+    {
+      "day": "wed",
+      "amount": 52.36
+    },
+    {
+      "day": "thu",
+      "amount": 31.07
+    },
+    {
+      "day": "fri",
+      "amount": 23.39
+    },
+    {
+      "day": "sat",
+      "amount": 43.28
+    },
+    {
+      "day": "sun",
+      "amount": 25.48
+    }
+  ]
+
+function init() {
+    let barDivs = document.querySelectorAll(".bar");
+    let totalAmt = 0;
+
+    for(let i = 0; i< data.length; i++) {
+        totalAmt += data[i].amount;
+    }
+
+    for(let i = 0; i < barDivs.length; i++) {
+        let amt = data[i].amount;
+
+        console.log(amt/totalAmt * 100 + "%")
+        barDivs[i].style.height = amt/totalAmt * 100 + "%";
+        barDivs[i].parentElement.querySelector(".bar-value").innerHTML = "$" + amt;
+        barDivs[i].addEventListener("click", displayValue);
+    }
+}
+
+function displayValue(e) {
+    let valueDiv = e.target.parentElement.querySelector(".bar-value");
+    let active = e.target.classList.contains('active') ? true : false;
+
+
+
+    if(active) {
+        valueDiv.style.visibility = "hidden";
+        e.target.classList.remove('active');
+    }
+    else {
+        valueDiv.style.visibility = "visible";
+        e.target.classList.add('active');
+    }
+    
+}
